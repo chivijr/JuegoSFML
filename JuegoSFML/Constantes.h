bd94c06da2813333f35ecda64d66cf5584bc8710
Chivijr <Chivijr@DESKTOP-OC7OGPE>
2017-12-24 04:41:38 +0100

    Añado Box2D para la física del juego.

--- JuegoSFML/Constantes.h
+++ JuegoSFML/Constantes.h
@@ -1,7 +1,12 @@
 #pragma once
 #define logdebug false
-#define cAceleracion 1
+#define cAceleracion 5
 #define altoPantalla 600
 #define anchoPantalla altoPantalla*1.33
+#define tituloPantalla "Juego"
 #define rutaProtagonista "C:/Users/Chivijr/source/repos/JuegoSFMLSolucion/Debug/recursos/sprite/prota/walk01.png"
 #define rutaProfesor "C:/Users/Chivijr/source/repos/JuegoSFMLSolucion/Debug/recursos/sprite/maestro/idle01.png"
+#define rutaFondo "C:/Users/Chivijr/source/repos/JuegoSFMLSolucion/Debug/recursos/working-title-assets/untitled folder/exterior-parallaxBG1.png"
+
+//Variables Box2D
+#define SCALE 2
